a5d9bf527aaf8e310ab93346bd7f1a869eb8e84c
bsrAkdg <busraaakdag@gmail.com>
2020-09-19 13:30:56 +0300

    Handling request permissions

--- TrackerAppWithGoogleMaps/app/src/main/java/com/bsrakdg/trackerappwithgooglemaps/ui/fragments/RunFragment.kt
+++ TrackerAppWithGoogleMaps/app/src/main/java/com/bsrakdg/trackerappwithgooglemaps/ui/fragments/RunFragment.kt
@@ -1,5 +1,7 @@
 package com.bsrakdg.trackerappwithgooglemaps.ui.fragments
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -7,8 +9,12 @@ import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.bsrakdg.trackerappwithgooglemaps.R
 import com.bsrakdg.trackerappwithgooglemaps.ui.viewmodels.MainViewModel
+import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import com.bsrakdg.trackerappwithgooglemaps.utils.TrackingUtil
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_run.*
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
 
 /**
  * While injecting viewModels int the fragment we do not have to write inject
@@ -17,16 +23,65 @@ import kotlinx.android.synthetic.main.fragment_run.*
  * and assign it to this viewModel variable here.
  */
 @AndroidEntryPoint
-class RunFragment : Fragment(R.layout.fragment_run) {
+class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionCallbacks {
 
     private val viewModel: MainViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requestPermissions()
 
         fab.setOnClickListener {
             findNavController().navigate(R.id.action_runFragment_to_trackingFragment)
         }
     }
+
+    private fun requestPermissions() {
+        if (TrackingUtil.hasLocationPermissions(requireContext())) {
+            return
+        }
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "You need to accept location permissions to use this app.",
+                REQUEST_CODE_LOCATION_PERMISSION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            )
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
 }
---
+++ TrackerAppWithGoogleMaps/app/src/main/java/com/bsrakdg/trackerappwithgooglemaps/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.bsrakdg.trackerappwithgooglemaps.utils
+
+object Constants {
+
+    const val REQUEST_CODE_LOCATION_PERMISSION = 0
+}
