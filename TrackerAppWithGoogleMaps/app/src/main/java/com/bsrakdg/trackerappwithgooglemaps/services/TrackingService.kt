115d41fe29bd9ed10902fd5cdfa44eb6d1dc6b22
bsrAkdg <busraaakdag@gmail.com>
2020-10-04 13:33:10 +0300

    Tracking user location in the background

--- TrackerAppWithGoogleMaps/app/src/main/java/com/bsrakdg/trackerappwithgooglemaps/services/TrackingService.kt
+++ TrackerAppWithGoogleMaps/app/src/main/java/com/bsrakdg/trackerappwithgooglemaps/services/TrackingService.kt
@@ -1,5 +1,6 @@
 package com.bsrakdg.trackerappwithgooglemaps.services
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
@@ -7,12 +8,16 @@ import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MutableLiveData
 import com.bsrakdg.trackerappwithgooglemaps.R
 import com.bsrakdg.trackerappwithgooglemaps.ui.MainActivity
+import com.bsrakdg.trackerappwithgooglemaps.utils.Constants
 import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.ACTION_PAUSE_SERVICE
 import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.ACTION_START_OR_RESUME_SERVICE
@@ -20,15 +25,50 @@ import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.ACTION_STOP_SERVICE
 import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.NOTIFICATION_CHANNEL_ID
 import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.NOTIFICATION_CHANNEL_NAME
 import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.NOTIFICATION_ID
+import com.bsrakdg.trackerappwithgooglemaps.utils.TrackingUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
 
 /**
  * We are going to listen LiveData objects from this service.
  * For this reason, this class should be a LifecycleService.
  */
+
+typealias Polyline = MutableList<LatLng> // ...
+typealias Polylines = MutableList<Polyline> // ... ... ...
+
 class TrackingService : LifecycleService() {
 
     var isFirstRun = true
+
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    private fun postInitialValues() {
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf()) // empty list
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValues()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
+
+        // this class extends from LifecycleService, you can observe livedata
+        isTracking.observe(this, {
+            updateLocationTracking(it)
+        })
+    }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         // Sending intent from Fragment to Service to communication
@@ -55,7 +95,66 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    // requestLocationUpdates function doesn't handle third party request permission
+    // so use SuppressLint to ignore this reason
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (TrackingUtil.hasLocationPermissions(this)) {
+                val request = LocationRequest().apply {
+                    interval = Constants.LOCATION_UPDATE_INTERVAL
+                    fastestInterval = Constants.FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        } else {
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    // Handle location changes and when user change location, if user is tracking, add to list.
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult?) {
+            super.onLocationResult(result)
+
+            if (isTracking.value!!) { // if we are currently tracking
+                result?.locations?.let { locations ->
+                    locations.map {
+                        addPathPoint(it) // we add each new point to list
+                        Timber.d(("NEW LOCATION: ${it.latitude}, ${it.longitude}"))
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos) // add new path point to end of the pathPoints
+                pathPoints.postValue(this) // when used apply, you can use this
+            }
+        }
+    }
+
+    // when user click Stop button while tracking, this method adds an empty polyline in our pathPoints list.
+    private fun addEmptyPolyline() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this) // empty list, when used apply, you can use this
+    }
+        ?: pathPoints.postValue(mutableListOf(mutableListOf())) // when pathPoints is null, initial empty pathPoints list.
+
     private fun startForegroundService() {
+        addEmptyPolyline() // first empty polyline
+        isTracking.postValue(true)
+
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
