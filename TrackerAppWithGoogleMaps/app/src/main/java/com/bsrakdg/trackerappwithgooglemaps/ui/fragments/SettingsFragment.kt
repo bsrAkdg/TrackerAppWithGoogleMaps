70f2f114c5abe30e02e77eb0e01d5cc1df597e1e
bserdaroglu <busraaakdag@gmail.com>
2021-01-29 22:35:43 +0300

    Total run statistics & settings fragment

--- TrackerAppWithGoogleMaps/app/src/main/java/com/bsrakdg/trackerappwithgooglemaps/ui/fragments/SettingsFragment.kt
+++ TrackerAppWithGoogleMaps/app/src/main/java/com/bsrakdg/trackerappwithgooglemaps/ui/fragments/SettingsFragment.kt
@@ -1,8 +1,63 @@
 package com.bsrakdg.trackerappwithgooglemaps.ui.fragments
 
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.bsrakdg.trackerappwithgooglemaps.R
+import com.bsrakdg.trackerappwithgooglemaps.databinding.FragmentSettingsBinding
+import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.KEY_NAME
+import com.bsrakdg.trackerappwithgooglemaps.utils.Constants.KEY_WEIGHT
+import com.google.android.material.snackbar.Snackbar
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class SettingsFragment : Fragment(R.layout.fragment_settings) {
 
+    private lateinit var binding: FragmentSettingsBinding
+
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
+
+    private fun loadFieldsFromSharedPref() {
+        val name = sharedPreferences.getString(KEY_NAME, "")
+        val weight = sharedPreferences.getFloat(KEY_WEIGHT, 80F)
+        binding.etName.setText(name)
+        binding.etWeight.setText(weight.toString())
+    }
+
+    private fun applyChangesToSharedPref(): Boolean {
+        val nameText = binding.etName.text.toString()
+        val weightText = binding.etWeight.text.toString()
+
+        if (nameText.isEmpty() || weightText.isEmpty()) {
+            return false;
+        }
+
+        sharedPreferences.edit()
+            .putString(KEY_NAME, nameText)
+            .putFloat(KEY_WEIGHT, weightText.toFloat())
+            .apply()
+        val toolbarText = "Let's go $nameText"
+        requireActivity().findViewById<TextView>(R.id.tvToolbarTitle)?.text = toolbarText
+        return true
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSettingsBinding.bind(view)
+
+        loadFieldsFromSharedPref()
+
+        binding.btnApplyChanges.setOnClickListener {
+            val success = applyChangesToSharedPref()
+            if (success) {
+                Snackbar.make(view, getString(R.string.saved_changes), Snackbar.LENGTH_LONG).show()
+            } else {
+                Snackbar.make(view, getString(R.string.fill_out_fields), Snackbar.LENGTH_LONG).show()
+            }
+        }
+    }
 }
--- TrackerAppWithGoogleMaps/app/src/main/java/com/bsrakdg/trackerappwithgooglemaps/ui/fragments/StatisticsFragment.kt
+++ TrackerAppWithGoogleMaps/app/src/main/java/com/bsrakdg/trackerappwithgooglemaps/ui/fragments/StatisticsFragment.kt
@@ -1,14 +1,57 @@
 package com.bsrakdg.trackerappwithgooglemaps.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bsrakdg.trackerappwithgooglemaps.R
+import com.bsrakdg.trackerappwithgooglemaps.databinding.FragmentStatisticsBinding
 import com.bsrakdg.trackerappwithgooglemaps.ui.viewmodels.StatisticsViewModel
+import com.bsrakdg.trackerappwithgooglemaps.utils.TrackingUtil
 import dagger.hilt.android.AndroidEntryPoint
+import kotlin.math.round
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
+    private lateinit var binding: FragmentStatisticsBinding
+
     private val viewModel: StatisticsViewModel by viewModels()
 
+    private fun subscribeToObserver() {
+        viewModel.totalTimeRun.observe(viewLifecycleOwner, {
+            it?.let {
+                val totalTimeRun = TrackingUtil.getFormattedStopWatchTime(it)
+                binding.tvTotalTime.text = totalTimeRun
+            }
+        })
+        viewModel.totalDistance.observe(viewLifecycleOwner, {
+            it?.let {
+                val km = it / 1000f
+                val totalDistance = round(km * 10f) / 10f
+                val totalDistanceString = "${totalDistance}km"
+                binding.tvTotalDistance.text = totalDistanceString
+            }
+        })
+        viewModel.totalAvgSpeed.observe(viewLifecycleOwner, {
+            it?.let {
+                val avgSpeed = round(it * 10f) / 10f
+                val avgSpeedString = "${avgSpeed}km/h"
+                binding.tvAverageSpeed.text = avgSpeedString
+            }
+        })
+        viewModel.totalCaloriesBurned.observe(viewLifecycleOwner, {
+            it?.let {
+                val totalCalories = "${it}kcal"
+                binding.tvTotalCalories.text = totalCalories
+            }
+        })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentStatisticsBinding.bind(view)
+
+        subscribeToObserver()
+    }
 }
